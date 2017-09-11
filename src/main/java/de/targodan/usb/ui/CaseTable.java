6cd1ab502704b4568aa27e40e9b627b679b8f382
Luca Corbatto <luca@corbatto.de>
2017-09-11 20:08:18 +0200

    Fixed line height on new cases (again)

--- src/main/java/de/targodan/usb/ui/CaseTable.java
+++ src/main/java/de/targodan/usb/ui/CaseTable.java
@@ -162,6 +162,11 @@ public class CaseTable extends JTable {
                 panel.setText("("+panel.getText()+")");
             }
             
+            int height = panel.getPreferredSize().height;
+            if(height > 0 && table.getRowHeight(row) < height) {
+                table.setRowHeight(row, height);
+            }
+            
             return panel;
         }
     }
